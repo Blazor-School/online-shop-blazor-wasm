79caa6947684b138beaf9923bebf97f95b7135bb
Red Wei <hevilhuy2@gmail.com>
2021-09-16 11:00:00 +0700

    Add checkout controller.

---
+++ OnlineShopApi/Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopApi.Models;
+
+namespace OnlineShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckoutController : ControllerBase
+    {
+        [Route("[action]")]
+        [HttpPost]
+        public IActionResult Checkout(CheckoutInfo checkoutInfo)
+        {
+            return Ok($"Thank you {checkoutInfo.Name}, we will deliver to {checkoutInfo.Address}.");
+        }
+    }
+}
